ed2ad86b8c75c309a15af74d6e5784b20b0ec41c
maheshmahi712 <mahesh461.workmail@gmail.com>
2026-08-28 18:53:58 +0530

    Updated HelloController.java

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,1 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello! Spring Boot Application is Running 🚀";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Spring Boot!";
+    }
+}
 

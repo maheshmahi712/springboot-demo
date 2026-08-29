c83767fa4b85bd03900c40b5d7b871d46ff62218
maheshmahi712 <mahesh461.workmail@gmail.com>
2026-08-29 18:23:52 +0530

    Update HelloController.java

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String home() {
-        return "Hello! Spring Boot Application is Running 🚀";
+        return "Hello! Spring Boot Application is Runningggggggguuuuuu 🚀";
     }
 
     @GetMapping("/hello")
